e7ea81982a7fe64050d183c1b86b805d2342a529
mdshahjhan <mdshahjha478877@gmail.com>
2024-05-31 10:55:47 +0600

    fulstack

--- src/errorhandler.ts
+++ src/errorhandler.ts
@@ -1,1 +1,20 @@
 
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-const */
+import { Request,Response,NextFunction } from "express";
+import httpStatus from "http-status";
+
+
+const notFound=((req:Request,res:Response,next:NextFunction)=>{
+
+    
+    return res.status(httpStatus.NOT_FOUND).json({
+        success: false,
+        message: "Order not found"
+  
+    })
+})
+
+export  default  notFound
